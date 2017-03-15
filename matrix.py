6b1e64421e68c144f0356b0a1aba1fb299373e20
Xinhui <x.xu.1102@gmail.com>
2017-03-15 15:22:01 -0400

    make b h

--- matrix.py
+++ matrix.py
@@ -1,10 +1,43 @@
 import math
 
 def make_bezier():
-    pass
+    t = new_matrix()
+    t[0][0] = -1
+    t[0][1] = 3
+    t[0][2] = -3
+    t[0][3] = 1
+    t[1][0] = 3
+    t[1][1] = -6
+    t[1][2] = 3
+    t[2][0] = -3
+    t[2][1] = -3
+    t[3][0] = 1
+    print_matrix(t)
+    return t
 
 def make_hermite():
-    pass
+    t = new_matrix()
+    t[0][0] = 2
+    t[0][1] = -3
+    t[0][3] = 1
+    t[1][0] = -2
+    t[1][1] = 3
+    t[2][0] = 1
+    t[2][1] = -2
+    t[2][2] = 1
+    t[3][0] = 1
+    t[3][1] = -1
+    print_matrix(t)
+    return t
+
+'''
+2 -2 1 1
+-3 3 -2 -1
+0 0 1 0
+1 0 0 0
+'''
+
+
 
 def generate_curve_coefs( p1, p2, p3, p4, t ):
     pass
@@ -97,3 +130,6 @@ def new_matrix(rows = 4, cols = 4):
         for r in range( rows ):
             m[c].append( 0 )
     return m
+
+make_hermite()
+make_bezier()
